497cd0d8ca4195676270bdd2673442bde3c68bc6
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-08-28 12:30:17 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,29 @@
+import sys
+
+while True:
+    data = sys.stdin.readline()
+    if data.replace("\n", "")==".":
+        break
+
+    stack = []
+
+    for i in data:
+        if (i=="[" or i=="("):
+            stack.append(i)
+        elif (i=="]"):
+            if(len(stack)!=0 and stack[-1]=="["):
+                stack.pop()
+            else:
+                stack.append(i)
+                break
+        elif (i==")"):
+            if(len(stack)!=0 and stack[-1]=="("):
+                stack.pop()
+            else:
+                stack.append(i)
+                break
+    
+    if(len(stack)==0):
+        print("yes")
+    else:
+        print("no")
