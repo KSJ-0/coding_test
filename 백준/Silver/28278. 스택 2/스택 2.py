7b8cfcc93722c1dcc939b4505e595361cc0e7abd
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-08-28 11:03:19 +0900

    [Silver IV] Title: 스택 2, Time: 556 ms, Memory: 88788 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -3,28 +3,30 @@ input = sys.stdin.readline
 
 N = int(input().strip())
 stack = []
+result = []
 
 for _ in range(N):
     order = input().split()
-    if order[0]=='1':
+    order_num = order[0]
+    if order_num == '1':
         stack.append(order[1])
-    elif order[0]=='2':
-        if len(stack)!=0:
-            print(stack.pop())
+    elif order_num == '2':
+        if len(stack) != 0:
+            result.append(stack.pop())
         else:
-            print('-1')
-    elif order[0]=='3':
-        print(str(len(stack)))
-    elif order[0]=='4':
-        if len(stack)!=0:
-            print('0')
+            result.append('-1')
+    elif order_num == '3':
+        result.append(str(len(stack)))
+    elif order_num == '4':
+        if len(stack) != 0:
+            result.append('0')
         else:
-            print('1') 
-    elif order[0]=='5':
-        if len(stack)!=0:
-            top = stack.pop()
-            print(top)
-            stack.append(top)      
+            result.append('1') 
+    elif order_num == '5':
+        if len(stack) != 0:
+            result.append(stack[-1])    
         else:
-            print('-1')
+            result.append('-1')
+            
+sys.stdout.write('\n'.join(result))            
        
