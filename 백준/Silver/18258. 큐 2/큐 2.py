0b06d1dbcef06a8cf69ee2c36faaae368835c5fd
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-09-01 11:16:33 +0900

    [Silver IV] Title: 큐 2, Time: 1140 ms, Memory: 143272 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,34 +1,24 @@
 import sys
 from collections import deque
-N = int(sys.stdin.readline().strip())
-queue = deque()
-for _ in range(N):
-    order = sys.stdin.readline().split()
-    if order[0] == "push":
-        queue.append(order[1])
-    elif order[0] == "pop":
-        if queue:
-            print(queue.popleft())
-        else:
-            print("-1")
-    elif order[0] == "size":
-        print(len(queue))
-    elif order[0] == "empty":
-        if queue:
-            print("0")
-        else:
-            print("1")
-    elif order[0] == "front":
-        if queue:
-            top = queue.popleft()
-            queue.appendleft(top)
-            print(top)
-        else:
-            print("-1")
-    elif order[0] == "back":
-        if queue:
-            bottom = queue.pop()
-            queue.append(bottom)
-            print(bottom)
-        else:
-            print("-1")
+input = sys.stdin.readline
+
+def solve():
+    N = int(input().strip())
+    queue = deque()
+    for _ in range(N):
+        order = input().split()
+        if order[0] == "push":
+            queue.append(order[1])
+        elif order[0] == "pop":
+            print(-1 if not queue else queue.popleft())
+        elif order[0] == "size":
+            print(len(queue))
+        elif order[0] == "empty":
+            print(1 if not queue else 0)
+        elif order[0] == "front":
+            print(-1 if not queue else queue[0])
+        elif order[0] == "back":
+            print(-1 if not queue else queue[-1])
+
+if __name__ == "__main__":
+    solve()
