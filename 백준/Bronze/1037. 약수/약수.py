7a533d39d2026c07e9444cd3a2634262e4391745
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-09-08 10:19:22 +0900

    [Bronze I] Title: 약수, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.py
+++ 백준/Bronze/1037. 약수/약수.py
@@ -1,8 +1,6 @@
 import sys
-N = int(sys.stdin.readline().strip())
-num = list(map(int, sys.stdin.readline().split()))
-num.sort()
-if N==1:
-    print(num[0]**2)
-else:
-    print(num[0]*num[N-1])
+input = sys.stdin.readline
+N = int(input().strip())
+num = list(map(int, input().split()))
+
+print(min(num)*max(num))
