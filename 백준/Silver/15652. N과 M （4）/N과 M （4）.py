fdc8ee385088c28ca195a0eb6c576719afb58dd5
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-09-22 11:57:53 +0900

    [Silver III] Title: N과 M (4), Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -2,20 +2,16 @@ import sys
 N, M = map(int, sys.stdin.readline().split())
 result = []
 output = []
-last_used = 0
 
-def backtrack():
+def backtrack(start):
     global last_used
     if len(result) == M:
         output.append(' '.join(map(str, result)))
         return
-    for i in range(1, N+1):
-        if last_used <= i:
-            result.append(i)
-            last_used = i
-            backtrack()
-            last_used = 0
-            result.pop()
+    for i in range(start, N+1):
+        result.append(i)
+        backtrack(i)
+        result.pop()
 
-backtrack()
+backtrack(1)
 print('\n'.join(output))
