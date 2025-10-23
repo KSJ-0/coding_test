f39b9eb5d3829f410607d2cc7940554543ea5fb4
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-10-23 11:29:19 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 368 ms, Memory: 153132 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -6,23 +6,28 @@ s_len = len(S)
 q = int(input().strip())
 dic = {}
 
+for c in range(ord('a'), ord('z')+1):
+    dic[int(c)] = [0]*(s_len+1)
+
+
 for i in range(s_len):
-    alpha = S[i]
-    if alpha not in dic:
-        dic[alpha] = [0]*(s_len+1)
-    
-    for j in range(i, s_len+1):
-        dic[alpha][j] = dic[alpha][i-1] + 1
+    k = ord(S[i])
+    for alpha in dic:
+        if alpha == k: 
+            dic[alpha][i+1] = dic[alpha][i] + 1
+        else:
+            dic[alpha][i+1] = dic[alpha][i]
 
 for _ in range(q):
     a, l, r = input().split()
+    a = ord(a)
     l = int(l)
     r = int(r)
 
     if a in dic:
         if l == 0:
-            print(dic[a][r])
+            print(dic[a][r+1])
         else:
-            print(dic[a][r]-dic[a][l-1])
+            print(dic[a][r+1]-dic[a][l])
     else:
         print(0)
