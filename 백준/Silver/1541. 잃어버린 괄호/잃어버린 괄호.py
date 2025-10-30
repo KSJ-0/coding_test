a2e411312203270cd41065efabe028f0b6e937e6
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-10-30 11:48:47 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,15 +1,13 @@
 import sys
 p = sys.stdin.readline().split("-")
 result = 0
+values = p[0].split("+")
 if len(p) == 1:
-    values = p[0].split("+")
     result = sum(map(int,values))
 else:
-    values = p[0].split("+")
     for value in values:
             result += int(value)
     for i in range(1, len(p)):
-        values = p[i].split("+")
-        for value in values:
-            result -= int(value)
+        minus_values = p[i].split("+")
+        result -= sum(map(int, minus_values))
 print(result)
