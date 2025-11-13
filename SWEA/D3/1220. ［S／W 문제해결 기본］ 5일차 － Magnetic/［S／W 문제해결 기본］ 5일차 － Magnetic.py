09975064ceaf8156fe3a8e3fffd6c8cf3241c038
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-11-13 11:32:02 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 104 ms, Memory: 62,720 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
@@ -2,15 +2,14 @@ for i in range(1, 11):
     N = int(input().strip())
     table = [input().split() for _ in range(N)]
     deadlock = 0
-    before = ""
-    for n in range(N):
+    
+    for col in range(N):
         one = False
-        for m in range(N):
-            if "1" == table[m][n]:
+        for row in range(N):
+            if "1" == table[row][col]:
                 one = True
-                before = "1"
-            elif "2" == table[m][n] and one == True:
-                if before is not "2":
-                    deadlock += 1    
-                before = "2"
-    print(f"#{i} {deadlock}")    
+            elif "2" == table[row][col] and one == True:
+                deadlock += 1    
+                one = False
+                
+    print(f"#{i} {deadlock}")
