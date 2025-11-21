ddde7c725051fbcb33e6a2ad5819275fc59dba7d
KSJ-0 <130030337+KSJ-0@users.noreply.github.com>
2025-11-21 11:09:26 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 111 ms, Memory: 60,672 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
@@ -8,14 +8,13 @@ for _ in range(10):
         maximum = max(maximum, sum(arr))
 
     #열의 합
-    numArrRe = [[numArr[j][i] for j in range(100)] for i in range(100)]
-    for arrRe in numArrRe:
-        maximum = max(maximum, sum(arrRe))
+    numArrRe = [sum([numArr[j][i] for j in range(100)]) for i in range(100)]
+    maximum = max(maximum, max(numArrRe))
 
     #대각선의 합
     crossR = [numArr[l][l] for l in range(100)]
     maximum = max(maximum, sum(crossR))
-    crossL = [numArr[100-1-k][k] for k in range(100)]
+    crossL = [numArr[k][100-1-k] for k in range(100)]
     maximum = max(maximum, sum(crossL))
     
     print(f"#{testNum} {maximum}")
